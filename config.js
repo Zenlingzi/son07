d8328cd7f84e16532473f6bad8bef9d294bff349
Zenlingzi <91784694+Zenlingzi@users.noreply.github.com>
2021-11-18 17:41:09 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,16 @@
+const CONFIG = {
+    introTitle: 'Cậu kia!',
+    introDesc: `Tớ cần câu trả lời thật lòng từ cậu`,
+    btnIntro: 'OK',
+    title: 'Cậu có thích tớ không',
+    desc: 'Đừng nói dối nhé? ',
+    btnYes: 'Kó',
+    btnNo: 'ko',
+    question:'Nói cho tớ lý do cậu thích tớ đi :Đ',
+    btnReply: 'Gửi cho tớ <3',
+    reply: 'Vì cậu xứng đáng',
+    mess: 'Love you 3000 ^^',
+    messDesc: 'Tớ rất vui, mình ib nhau nhé!',
+    btnAccept: 'Okiiiii lun <3',
+    messLink: 'https://www.youtube.com/watch?v=46pra8NwhzU'
+}
